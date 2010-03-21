09dd28e7de48763ccfc7c368a3f4f3a224c1d16c
Randall Degges <rdegges@gmail.com>
2010-03-21 00:34:19 -0700

    Fixing comment styles, and upping version number.

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
-#
-# setup.py
-#
-# @author:	Randall Degges
-# @email:	rdegges@gmail.com
-# @date:	10-20-09
-# @license:	GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
-#
-# This file is used to build, install, and package pycall.
+
+"""
+setup.py
+
+@author:	Randall Degges
+@email:		rdegges@gmail.com
+@date:		10-20-09
+@license:	GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
+
+This file is used to build, install, and package pycall.
+"""
 
 from distutils.core import setup
 
 setup(
 	name = 'pycall',
-	version = '1.4',
+	version = '1.5',
 	packages = ['pycall'],
 	author = 'Randall Degges',
 	author_email = 'rdegges@gmail.com',
