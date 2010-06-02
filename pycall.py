e94e120bc2b3a7904c0f2f89a56a82d2f5e02afe
Randall Degges <rdegges@gmail.com>
2010-06-01 20:02:25 -0700

    Updating docstring for `__buildfile`.

--- pycall.py
+++ pycall.py
@@ -78,7 +78,8 @@ class CallFile:
 		"""
 		Use the class attributes to build a call file string.
 
-		:return:	A call file string, ready to be written to a file.
+		:return:	A list which contains one call file directive in each
+					element. These can be written to a file.
 		"""
 		if not __is_valid():
 			raise UnknownError
