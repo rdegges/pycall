a39ca403e0dbe7d1a9013686f3833dc4a5e88dc1
Randall Degges <rdegges@gmail.com>
2010-07-31 13:35:06 -0700

    Adding a `CallFile` import to `__init__.py`.
    This will let developers do:
    
    from pycall import CallFile
    
    Instead of the ugly way it was previously done:
    
    from pycall.callfile import CallFile
    
    Which just looks ugly. This new way is simpler, more elegant, and sexier.

---
+++ pycall/__init__.py
@@ -0,0 +1,12 @@
+"""
+	pycall
+	~~~~~~
+
+	A flexible python library for creating and using Asterisk call files.
+
+	:copyright: (c) 2010 by Randall Degges.
+	:license: BSD, see LICENSE for more details.
+"""
+
+
+from .callfile import CallFile
