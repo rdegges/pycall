82f196d1dfb092e0c21cd1751097a2cd8980d1fe
Randall Degges <rdegges@gmail.com>
2011-01-22 22:14:17 -0800

    Adding a basic test for creating CallFile objects.

--- tests/unit/test_pycall.py
+++ tests/unit/test_pycall.py
@@ -1,1 +1,16 @@
 """Unit tests for `pycall`."""
+
+from unittest import TestCase
+
+from nose.tools import raises
+
+from pycall import CallFile
+
+
+class TestCallFile(TestCase):
+	"""Run tests on the `CallFile` class."""
+
+	@raises(TypeError)
+	def test_create_callfile(self):
+		"""Ensure creating an empty `CallFile` object fails."""
+		CallFile()
