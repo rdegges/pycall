a7feaf1e83b6d45289e0718394c81c0ebb5b40d2
Randall Degges <rdegges@gmail.com>
2011-01-22 17:20:01 -0800

    Updating docstring.

--- pycall/__init__.py
+++ pycall/__init__.py
@@ -1,12 +1,4 @@
-"""
-	pycall
-	~~~~~~
-
-	A flexible python library for creating and using Asterisk call files.
-
-	:copyright: (c) 2010 by Randall Degges.
-	:license: BSD, see LICENSE for more details.
-"""
+"""A flexible python library for creating and using Asterisk call files."""
 
 
 from errors import *
