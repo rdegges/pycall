6c6b08a63b308690144d73f54b98000e3b1b5672
Randall Degges <rdegges@gmail.com>
2011-01-22 17:13:18 -0800

    Adding test case stub.

--- tests/unit/actions.py
+++ tests/unit/actions.py
@@ -1,1 +1,8 @@
 """Unit tests for `pycall.actions`."""
+
+
+class TestActions(TestCase):
+	"""Test all `pycall.actions` classes to ensure they are actual
+	`pycall.actions.Action` subclasses.
+	"""
+	pass
