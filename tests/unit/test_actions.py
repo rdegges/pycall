ec0330aa9742b138fff48ad01d00439099b7b8ea
Randall Degges <rdegges@gmail.com>
2011-01-22 17:56:00 -0800

    Adding attribute tests.

--- tests/unit/test_actions.py
+++ tests/unit/test_actions.py
@@ -3,7 +3,7 @@
 
 from unittest import TestCase
 
-from nose.tools import raises
+from nose.tools import eq_, raises
 
 from pycall import Application, Context
 
@@ -16,6 +16,12 @@ class TestApplication(TestCase):
 		"""Ensure creating an empty `Application` object fails."""
 		Application()
 
+	def test_application_attrs(self):
+		"""Ensure that all `Application` attributes stick."""
+		a = Application('Playback', 'hello-world')
+		eq_(a.application, 'Playback')
+		eq_(a.data, 'hello-world')
+
 
 class TestContext(TestCase):
 	"""Test the `pycall.actions.Context` class."""
@@ -24,3 +30,10 @@ class TestContext(TestCase):
 	def test_create_context(self):
 		"""Ensure creating an empty `Context` object fails."""
 		Context()
+
+	def test_context_attrs(self):
+		"""Ensure that all `Context` attributes stick."""
+		c = Context('Callout', 's', '1')
+		eq_(c.context, 'Callout')
+		eq_(c.extension, 's')
+		eq_(c.priority, '1')
