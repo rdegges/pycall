f4cbb2e296bda80a4a4225d1245f5f00b5bb78fb
Randall Degges <rdegges@gmail.com>
2011-01-22 16:33:43 -0800

    Adding stub for `pycall.Applicaiton.__init__`.

--- pycall/application.py
+++ pycall/application.py
@@ -3,4 +3,11 @@
 
 class Application(object):
 	"""Stores and manipulates Asterisk applications and data."""
-	pass
+
+	def __init__(self, application, data):
+		"""Create a new `Application` object.
+
+		:param str application: Asterisk application.
+		:param str data: Asterisk application data.
+		"""
+		pass
