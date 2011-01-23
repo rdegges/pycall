84558a996f0465ad6fda3c2de6169e4133452993
Randall Degges <rdegges@gmail.com>
2011-01-22 17:49:08 -0800

    Using new imports ;)

--- pycall/__init__.py
+++ pycall/__init__.py
@@ -1,6 +1,6 @@
 """A flexible python library for creating and using Asterisk call files."""
 
 
-from errors import *
-from actions import *
-from callfile import *
+from .errors import *
+from .actions import *
+from .callfile import *
