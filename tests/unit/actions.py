9138112f3abef61a96485ade7e0b484a43429b81
Randall Degges <rdegges@gmail.com>
2011-01-22 17:14:53 -0800

    Revert "Adding test case stub."
    This reverts commit 6c6b08a63b308690144d73f54b98000e3b1b5672.

--- tests/unit/actions.py
+++ tests/unit/actions.py
@@ -1,8 +1,1 @@
 """Unit tests for `pycall.actions`."""
-
-
-class TestActions(TestCase):
-	"""Test all `pycall.actions` classes to ensure they are actual
-	`pycall.actions.Action` subclasses.
-	"""
-	pass
