06349d5d2044280fb6d3b4019855d23d68b3f7d9
Randall Degges <rdegges@gmail.com>
2011-01-22 17:21:52 -0800

    Adding tests for creating action objects.

--- tests/unit/actions.py
+++ tests/unit/actions.py
@@ -1,1 +1,26 @@
 """Unit tests for `pycall.actions`."""
+
+
+from unittest import TestCase
+
+from nose.tools import raises
+
+from pycall.actions import Application, Context
+
+
+class TestApplication(TestCase):
+	"""Test the `pycall.actions.Application` class."""
+
+	@raises(TypeError)
+	def test_create_application(self):
+		"""Ensure creating an empty `Application` object fails."""
+		Application()
+
+
+class TestContext(TestCase):
+	"""Test the `pycall.actions.Context` class."""
+
+	@raises(TypeError)
+	def test_create_context(self):
+		"""Ensure creating an empty `Context` object fails."""
+		Context()
