2079330cd13be413a58bd5617edea5f4e5b23c0b
Randall Degges <rdegges@gmail.com>
2011-01-29 17:06:37 -0800

    Simplifying `Application` tests.

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -11,17 +11,19 @@ from pycall import Application, Context
 class TestApplication(TestCase):
 	"""Run tests on the `Application` class."""
 
+	def setUp(self):
+		"""Setup some default variables for test usage."""
+		self.a = Application('application', 'data')
+
 	def test_attrs_stick(self):
 		"""Ensure attributes stick."""
-		a = Application('application', 'data')
-		eq_(a.application, 'application')
-		eq_(a.data, 'data')
+		eq_(self.a.application, 'application')
+		eq_(self.a.data, 'data')
 
-	def test_application_str(self):
-		"""Ensure the `__str__` method works."""
-		a = Application('Playback', 'hello-world')
-		ok_('Playback' in ''.join(a.__str__()) and
-				'hello-world' in ''.join(a.__str__()))
+	def test_str_valid_application(self):
+		"""Ensure `__str__` works using test data."""
+		ok_('application' in ''.join(self.a.__str__()) and
+				'data' in ''.join(self.a.__str__()))
 
 
 class TestContext(TestCase):
