d75e28c1e98fc88e6557953e8dd1a37d429cd79c
Randall Degges <rdegges@gmail.com>
2011-04-30 18:43:35 -0700

    Adding test case for spool method.
    This test ensures that the spool method raises InvalidTimeError if the user
    specifies as invalite time parameter to the spool method.
    
    This could be really helpful, especially if the developer is new to pycall, and
    doesn't realize that the time parameter is only valid if it is a datetime
    object.
    
    This could save a lot of frustration and debugging for developers later on.

--- tests/test_callfile.py
+++ tests/test_callfile.py
@@ -149,3 +149,11 @@ class TestCallFile(TestCase):
 		"""
 		c = CallFile(self.call, self.action, spool_dir='/')
 		c.spool()
+
+	@raises(InvalidTimeError)
+	def test_spool_time_no_user_time_error(self):
+		"""Ensure that `spool` raises `InvalidTimeError` if the user doesn't
+		specify a valid `time` parameter.
+		"""
+		c = CallFile(self.call, self.action, spool_dir=self.spool_dir)
+		c.spool(666)
