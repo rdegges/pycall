aeae0c057f972b2a3604126c902c3b127dbb1d9c
Randall Degges <rdegges@gmail.com>
2011-04-30 18:50:21 -0700

    Alphabetizing errors.

--- pycall/errors.py
+++ pycall/errors.py
@@ -7,22 +7,22 @@ from exceptions import Exception
 class PycallError(Exception):
 	pass
 
-class ValidationError(PycallError):
-	"""CallFile could not be validated."""
-
-class NoSpoolPermissionError(PycallError):
-	"""You do not have permission to spool this call file."""
-
 class InvalidTimeError(PycallError):
 	"""You must specify a valid datetime object for the spool method's time
 	parameter.
 	"""
 
-class UnknownError(PycallError):
-	"""Something must have gone horribly wrong."""
+class NoSpoolPermissionError(PycallError):
+	"""You do not have permission to spool this call file."""
+
+class NoUserError(PycallError):
+	"""User does not exist."""
 
 class NoUserPermissionError(PycallError):
 	"""You do not have permission to change the ownership of this call file."""
 
-class NoUserError(PycallError):
-	"""User does not exist."""
+class UnknownError(PycallError):
+	"""Something must have gone horribly wrong."""
+
+class ValidationError(PycallError):
+	"""CallFile could not be validated."""
